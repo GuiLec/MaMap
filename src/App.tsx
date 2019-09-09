3fe3e72cc62f7cd89b4f585e3ec96d4cb3283672
GuiLec <glecallier@yahoo.com>
2019-09-09 21:22:05 +0200

    Refacto: creation of pages and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, {Fragment} from 'react';
-import MapView from 'react-native-maps';
+import {MapPage} from './pages/MapPage';
 
 // API key iOS : AIzaSyBQnjjZLmlg0Z-oax_LEVkB0-OzJ9lhyDM
 // API key Android : AIzaSyBQnjjZLmlg0Z-oax_LEVkB0-OzJ9lhyDM
@@ -7,17 +7,7 @@ import MapView from 'react-native-maps';
 const App = () => {
   return (
     <Fragment>
-      <MapView
-        style={{flex: 1}}
-        region={{
-          latitude: 42.882004,
-          longitude: 74.582748,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-        showsUserLocation={true}
-        provider="google"
-      />
+      <MapPage />
     </Fragment>
   );
 };
---
+++ src/pages/MapPage/MapPage.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {BaseMap} from '../../Components/BaseMap';
+
+export const MapPage = () => {
+  return <BaseMap />;
+};

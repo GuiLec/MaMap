8ac43e7bffd433ec5e6597966e3a9eb02bd08716
GuiLec <glecallier@yahoo.com>
2019-09-19 20:16:55 +0200

    Create a markersList state in BaseMap

--- src/components/BaseMap/useBaseMap.ts
+++ src/components/BaseMap/useBaseMap.ts
@@ -1,13 +1,19 @@
-import {useEffect, createRef} from 'react';
+import {useEffect, createRef, useState} from 'react';
 import MapView from 'react-native-maps';
 import {Geolocation} from '../../lib/Geolocation';
-import {markersList} from '../../modules/markersList';
+import {markersList as initialMarkersList} from '../../modules/markersList';
 
 const longitudeDelta = 0.01;
 const latitudeDelta = 0.005;
 
+interface Marker {
+  latitude: number;
+  longitude: number;
+}
+
 export const useBaseMap = () => {
   const mapRef = createRef<MapView>();
+
   const centerMapOnMyLocation = async () => {
     const position = await Geolocation.instance.getCurrentPosition();
     const region = {
@@ -18,6 +24,9 @@ export const useBaseMap = () => {
     };
     mapRef.current && mapRef.current.animateToRegion(region, 500);
   };
+
+  const [markersList, setMarkersList] = useState<Marker[]>(initialMarkersList);
+
   useEffect(() => {
     (async () => {
       centerMapOnMyLocation();
